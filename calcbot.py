fc42031dd1cba580388cf3c51cf8d346a75c24fd
GromDv <46959198+GromDv@users.noreply.github.com>
2023-01-28 22:33:49 +0300

    Update calcbot.py

--- calcbot.py
+++ calcbot.py
@@ -1,19 +1,26 @@
-import random
 import telebot
 from telebot import types
 
+# Имя бота для поиска в телеграме: gbpysem9-2
+
 bot = telebot.TeleBot("token", parse_mode=None)
-# заменить на 5844258519: и AAH1P_G4oxMj8-OQxBpMmfjeWR1FZYEtE7A
+# заменить на 5844258519: + AAH1P_G4oxMj8-OQxBpMmfjeWR1FZYEtE7A
 
 hiMess = "Это калькулятор!"
+
+num1 = 0
+mun2 = 0
+flag1 = 0
+flag2 = 0
+CalcComplex = 0
 
 @bot.message_handler(commands = ['button'])
 def button_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    item1 = types.KeyboardButton("Считать")
-    item2 = types.KeyboardButton("Закончить")
-    markup.add(item1)
-    markup.add(item2)
+    item1 = types.KeyboardButton("Считать рациональные")
+    item2 = types.KeyboardButton("Считать комплексные")
+    item3 = types.KeyboardButton("Закончить")
+    markup.add(item1, item2, item3)
     bot.send_message(message.chat.id,"Посчитаем?",reply_markup=markup)
 
 @bot.message_handler(commands=['start', 'help'])
@@ -22,24 +29,21 @@ def send_welcome(message):
     button_message(message)
 
 def send_mess(chat, mess):
-    markup = types.ReplyKeyboardMarkup(row_width=5,resize_keyboard=True, one_time_keyboard=True)
-    item1 = types.KeyboardButton("1")
-    item2 = types.KeyboardButton("2")
-    item3 = types.KeyboardButton("3")
-    item4 = types.KeyboardButton("4")
-    item5 = types.KeyboardButton("5")
-    item6 = types.KeyboardButton("6")
-    item7 = types.KeyboardButton("7")
-    item8 = types.KeyboardButton("8")
-    item9 = types.KeyboardButton("9")
-    item10 = types.KeyboardButton("0")
-    item11 = types.KeyboardButton("+")
-    item12 = types.KeyboardButton("-")
-    item13 = types.KeyboardButton("*")
-    item14 = types.KeyboardButton("/")
-    item15 = types.KeyboardButton("=")
-    markup.add(item1,item2,item3,item4,item5,item6,item7,item8,item9,item10)
+    markup = types.ReplyKeyboardMarkup(row_width=4,resize_keyboard=True, one_time_keyboard=True)
+    item1 = types.KeyboardButton("+")
+    item2 = types.KeyboardButton("-")
+    item3 = types.KeyboardButton("*")
+    item4 = types.KeyboardButton("/")
+    markup.add(item1,item2,item3,item4)
     bot.send_message(chat, mess, reply_markup=markup)
+
+def oper_message(chat, mess):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    item1 = types.KeyboardButton("Считать рациональные")
+    item2 = types.KeyboardButton("Считать комплексные")
+    item3 = types.KeyboardButton("Закончить")
+    markup.add(item1, item2, item3)
+    bot.send_message(chat,mess,reply_markup=markup)
 
 def send_final(chat, mess):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -49,19 +53,54 @@ def send_final(chat, mess):
     
 @bot.message_handler(content_types='text')
 def message_reply(message):
-    global vsego, maxHod, player, FlagSprav
+    global num1, num2, flag1, flag2, CalcComplex
 
     if message.text=="Сначала":
         send_welcome(message)
-    elif message.text=="Считать":
-        send_mess(message.chat.id, "Начнем!")
+    elif message.text=="Считать рациональные":
+        CalcComplex = 0
+        num1 = 0
+        num2 = 0
+        send_mess(message.chat.id, "Введите первое число: ")
+        flag1 = 1
+    elif message.text=="Считать комплексные":
+        CalcComplex = 1
+        num1 = 0
+        num2 = 0
+        send_mess(message.chat.id, "Введите первое число: ")
+        flag1 = 1
 
     elif message.text=="Нет":
         send_final(message.chat.id,'Успехов!')
     elif message.text=="Закончить":
         send_final(message.chat.id,'Успехов!')
+    elif message.text=="+":
+        oper_message(message.chat.id, "Сумма = "+ str(num1 + num2))
+    elif message.text=="-":
+        oper_message(message.chat.id, "Разность = "+ str(num1 - num2))
+    elif message.text=="*":
+        oper_message(message.chat.id, "Произведение = "+ str(num1 * num2))
+    elif message.text=="/":
+        oper_message(message.chat.id, "Частное = "+ str(num1 / num2))
+        
 
     else:
-        pass
+        if flag1 == 1:
+            if CalcComplex == 0:
+                num1 = int(message.text)
+            else:
+                num1 = complex(message.text)
+            send_mess(message.chat.id, "Введите второе число: ")
+            flag1 = 0
+            flag2 = 1
+        elif flag2 == 1:
+            if CalcComplex == 0:
+                num2 = int(message.text)
+            else:
+                num2 = complex(message.text)
+            send_mess(message.chat.id, "Введите операцию: ")
+            flag2 = 0
+
+        
         
 bot.infinity_polling()
